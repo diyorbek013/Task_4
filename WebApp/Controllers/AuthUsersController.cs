bde6e3d49358acbb7f3a5a303fbc317faea7fe15
diyorbek00013@gmail.com <diyorbek00013@gmail.com>
2022-11-23 00:51:28 +0500

    Authanticated users table created

---
+++ WebApp/Controllers/AuthUsersController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApp.Areas.Identity.Data;
+
+namespace WebApp.Controllers
+{
+    [Authorize]
+    public class AuthUsersController : Controller
+    {
+        private readonly UsersDbContext _context;
+        public AuthUsersController(UsersDbContext context)
+        {
+            _context = context;
+        }
+        // GET: Transaction
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Users.ToListAsync());
+        }
+
+        // GET: Users/Delete
+        public async Task<IActionResult> Delete(string? id)
+        {
+            if (id == null || _context.Users == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Users
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        // POST: Users/Delete
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            if (_context.Users == null)
+            {
+                return Problem("Entity set 'UsersDbContext.Users'  is null.");
+            }
+            var user = await _context.Users.FindAsync(id);
+            if (user != null)
+            {
+                _context.Users.Remove(user);
+            }
+
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
