623f9b0d5704b8fca4e58690a636bb0da46a943b
diyorbek00013@gmail.com <diyorbek00013@gmail.com>
2022-11-23 00:38:32 +0500

    Add project files.

---
+++ WebApp/Areas/Identity/Data/Users.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace WebApp.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the Users class
+public class Users : IdentityUser
+{
+    [PersonalData]
+    [Column(TypeName = "nvarchar(100)")]
+    public string FirstName { get; set; } = string.Empty;
+    public DateTime LastLogin { get; set; } = DateTime.Now;
+    public DateTime RegTime { get; set; } = DateTime.Now;
+}
+
